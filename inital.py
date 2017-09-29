aecc2926d42f3c56f315d75c61b6faa6d3a09b0f
Qiaoyang Zheng <mrzhengqy@outlook.com>
2017-09-29 23:19:40 +0800

    final version
    The final  version submitted to the disseration

--- inital.py
+++ inital.py
@@ -3,138 +3,200 @@ import math
 import random
 Max = 999999999
 Min = 0.0000001
-excuc = [92.79,85.20,78.99,74.57,69.65,65.59,61.80,58.73,56.15,53.58,51.26,49.19,100.40,110.60]
 #define job
 class Job():
-    def __init__(self,id, bestfrequency, source):
+    def __init__(self,id,source,bestfrequency):
         self.id = id
-        self.source = source
+        self.resource = source
         self.best = bestfrequency
 
 class Machine():
-    def __init__(self,id, frequency, source):
+    def __init__(self,id, source,frequency):
         self.id = id
         self.source = source
         self.initalFrequency = frequency
 
-
-j1 = Job(1,50,1000)
-j2 = Job(2,200,2000)
-j3 = Job(3,150,3000)
-j4 = Job(4,400,2500)
-j5 = Job(5,300,1500)
 Joblist =[]
+
+j1 = Job(1,5,1000)
+j2 = Job(2,20,2000)
+j3 = Job(3,15,3000)
+j4 = Job(4,40,2500)
+j5 = Job(5,30,1500)
+j6 = Job(6,25,1800)
+j7 = Job(7,35,1700)
+j8 = Job(8,29,1937)
+j9 = Job(9,47,1354)
+j10= Job(10,35,2119)
 #for i in range (0,5):
 Joblist.append(j1)
 Joblist.append(j2)
 Joblist.append(j3)
 Joblist.append(j4)
 Joblist.append(j5)
-m1 = Machine(1,500,2000)
-m2 = Machine(2,600,1500)
-mlist =[]
-mlist.append(m1)
-mlist.append(m2)
+Joblist.append(j6)
+Joblist.append(j7)
+Joblist.append(j8)
+Joblist.append(j9)
+Joblist.append(j10)
+m1 = Machine(0,50,2000)
+m2 = Machine(1,60,1500)
+m3 = Machine(2,50,1700)
+mlist =[m1,m2,m3]
 solnum =5
-sol = [[0 for i in range(solnum)] for j in range(len(Joblist))]
+sol = [[0 for i in range(len(Joblist))] for j in range(solnum)]
 def Initailzation(Machinelist,Joblist,solnum):
 
     for i in range(0,solnum):
         for j in range(0,len(Joblist)):
             sol[i][j]=random.randint(1,len(Machinelist))
         print(sol[i])
-Initailzation(mlist,Joblist,solnum)
+
 p=1
-def Mutation(chrosome,p):
-    pob=random.uniform(0,1)
-    if(pob-p<0.0000001):
-        num =  random.randint(0,len(Joblist)-1)
-        chrosome[num] = random.randint(1,len(mlist))
-        print(chrosome)
+#Initailzation(mlist,Joblist,solnum)
+# Single point mutation
+def Mutation(population,p):
+    for i in range (0,len(population)):
+        pob=random.uniform(0,1)
+        if(pob-p<0.0000001):
+                choro = population[i]
+                num =  random.randint(0,len(Joblist)-1)
+                choro[num] = random.randint(1,len(mlist))
+                population[i] = choro
+    return population
+#Mutation(sol,p)
+#print(sol)
+def Crossover(population):
 
+    num1 = 0
+    num2 = 0
+    while(num1==num2):
+        num1 = random.randint(0, len(population)-1)
+        num2 = random.randint(0,len(population)-1)
+    Parent1 = population[num1]
+    Parent2 = population[num2]
+    offspring = []
+    mid = random.randint(0,len(Parent1))
+    for i in range (0,len(Parent1)):
+        if(i<mid):
+            offspring.append(Parent1[i])
+        else:
+            offspring.append(Parent2[i])
+    #print(mid,Parent1,Parent2,offspring)
+       # mid = chrosome[num1]
+       # chrosome[num1] = chrosome[num2]
+       # chrosome[num2] = mid
+        #print(chrosome)
+    return offspring
+#Crossover(sol)
 
-def Crossover(chrosome,p):
-    pob = random.uniform(0, 1)
-    if (pob - p < 0.0000001):
-
-        num1 = 0
-        num2 = 0
-        while(num1==num2):
-            num1 = random.randint(0, len(Joblist) - 1)
-            num2 = random.randint(0,len(Joblist)-1)
-        mid = chrosome[num1]
-        chrosome[num1] = chrosome[num2]
-        chrosome[num2] = mid
-        print(chrosome)
-for a in range(0,5):
-    print(a)
-    Crossover(sol[a],p)
-fitgroup=[]
 def Selection(population,fitgroup):
+  offspring = []
 
-  pob = random.uniform(0,1)
   sumfit =0
   count =0
+  bestfit = 0.0
+  bestnum = 0
+  worstnum = 0
   for i in range(0,len(fitgroup)):
-      sumfit = sumfit + fitgroup(i)
-  while(count<len(population)):
-        curfit = 0
+      sumfit = sumfit + fitgroup[i]
+      #print(fitgroup[i])
+
+      if(fitgroup[i]-bestfit>Min):
+          bestfit = fitgroup[i]
+          bestnum = i
+  #print(sumfit)
+  while(count<len(fitgroup)):
+        pob = random.uniform(0, 1)
+        curfit = 0.0
         count = count + 1
+        minfit = Max
         for i in range(0, len(fitgroup)):
             curfit = curfit+1.0/sumfit*fitgroup[i]
-            if (pob-curfit<0.0000001):
-                print(population[i])
+            if (pob-curfit<Min):
+                #print(i)
+                offspring.append(population[i])
+                #print(pob)
+                #print(curfit,i)
+                if(fitgroup[i]-minfit<Min):
+                    minfit = fitgroup[i]
+                    worstnum = i
+                break
 
-def corr(freq):
-    excucal = 0
-    if freq == 1200:
-        excucal = excuc[0]
-    elif freq == 1300:
-        excucal = excuc[1]
-    elif freq == 1400:
-        excucal = excuc[2]
-    elif freq == 1500:
-        excucal = excuc[3]
-    elif freq == 1600:
-        excucal = excuc[4]
-    elif freq == 1700:
-        excucal = excuc[5]
-    elif freq == 1800:
-        excucal = excuc[6]
-    elif freq == 1900:
-        excucal = excuc[7]
-    elif freq == 2000:
-        excucal = excuc[8]
-    elif freq == 2100:
-        excucal = excuc[9]
-    elif freq == 2200:
-        excucal = excuc[10]
-    elif freq == 2300:
-        excucal = excuc[11]
-    elif freq == 1100:
-        excucal = excuc[12]
-    elif freq == 1000:
-        excucal = excuc[13]
-    return excucal
+  offspring[worstnum]=population[bestnum]
+  return offspring
 
 
-def Evaulation(chrosome,a,b,m1,m2):
+def transfer(freq,best):
+    normal = 1+(freq-best)/100* 0.1
+    return normal
+
+def calPcost(freq):
+    power = (freq/1000)**3
+    return  power
+
+def DVFS(best,curfre):
+    if(best>curfre):
+        curfre= curfre+1
+        return curfre
+    elif(best==curfre):
+        return curfre
+    else:
+        curfre = curfre - 1
+        return curfre
+
+
+def Evaulation(chrosome,a,b):
     sol = chrosome
     curmac = []
     curjob = []
     curfre = 0
+    time = [0,0]
+    t_interval = 1
+    p_cost = 0
     for i in range(0,len(sol)):
-        curmac = mlist.index(i)
+        curmac = mlist[chrosome[i]-1]
         curfre = curmac.initalFrequency
+        curjob = Joblist[i]
+        jobSource = Joblist[i].resource
+        best = curjob.best
+        while(jobSource>0):
+         #print(jobSource)
+         cur_finish = transfer(best,curfre)
+         curfre=DVFS(best,curfre)
+         jobSource = jobSource-cur_finish
+         time[curmac.id-1]= time[curmac.id-1]+t_interval
+         p_cost = calPcost(curfre)
+    makespan = 0
+    for i  in range(0,len(time)):
+        if (time[i]>makespan):
+            makespan  = time[i]
+    fitness = 0
+    fitness = makespan
+    #print(fitness)
+    return fitness
 
-        curjob = Joblist.index(i)
-        JobSource = curjob.source
-        bestcurjobf =  curjob.best
-        
 
+#Main
 
+Initailzation(mlist,Joblist,solnum)
+fitgroup = [0 for i in range(len(sol))]
+x=1
+y=0
+final = []
 
+for a in range(0,5):
+    #print(sol)
 
+    Gene = []
+    for i in range(0,len(sol)):
+        mid = Crossover(sol)
+        Gene.append(mid)
+        fitgroup[i] = Evaulation(mid, x, y)
+        print("fitness",i,fitgroup[i])
+    Sol = Selection(Gene,fitgroup)
+    Mutation(sol, p)
+#print(sol)
 
 
 
